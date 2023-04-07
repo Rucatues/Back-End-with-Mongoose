c1f9250c64dba452aaccf55b2bfe2df9c1356edb
Rucatues <115330764+Rucatues@users.noreply.github.com>
2023-04-07 12:54:27 -0500

    commented out mongoose connect

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const path = require('path');
-const routes = require('./routes');
+// const routes = require('./routes');
 
 // middleware //
 
@@ -12,6 +12,15 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // connect to database with  Mongoose //
+
+// mongoose.connect( , { useNewURLParser: true })
+//     .then(() => {
+//         console.log("Mongoose Connection Open!")
+//     })
+//     .catch(err => {
+//         console.log("Mongoose connection not working")
+//         console.log(err)
+//     });
 
 // connect to PORT //
 app.listen(PORT, () => {
