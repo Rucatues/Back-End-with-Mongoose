d0798ed121730465a76156ea45a1a9cc7be38902
Rucatues <115330764+Rucatues@users.noreply.github.com>
2023-04-08 09:53:50 -0500

    started model for User

--- models/User.js
+++ models/User.js
@@ -1,20 +1,29 @@
-// username
-    // String
-    // Unique
-    // Required
-    // Trimmed
-
-// email
-    // String
-    // Required
-    // Unique
-    // Must match a valid email address (look into Mongoose's matching validation)
-
-// thoughts
-    // Array of _id values referencing the Thought model
-
-// friends
-    // Array of _id values referencing the User model (self-reference)
 
 // Schema Settings
-    // Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+// Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
+    },
+    // thoughts:
+    // Array of _id values referencing the Thought model
+
+    // friends: 
+    // Array of _id values referencing the User model (self-reference)
+
+
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
