14b45f6e86496df191bb3da14cc86f9818c60778
Rucatues <115330764+Rucatues@users.noreply.github.com>
2023-04-08 10:09:11 -0500

    added notes to routes with methods to call

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,16 +1,27 @@
 const router = require('express').Router();
+const Thought = require('../models/Thought');
 
 // -------------routes for /api/thoughts-------------
 
 // GET to get all thoughts
 
+// Thought.find({})
+
 // GET to get a single thought by its _id
+
+// Thought.findOne()
 
 // POST to create a new thought(don't forget to push the created thought's _id to the associated user's thoughts array field)
 
+// Thought.create()
+
 // PUT to update a thought by its _id
 
+// Thought.findOneAndUpdate()
+
 // DELETE to remove a thought by its _id
+
+// Thought.findOneAndDelete()
 
 // -------------routes for /api/thoughts/:thoughtId/reactions-------------
 
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,19 +4,30 @@ const router = require('express').Router();
 
 // GET all users
 
+// User.find({})
+
 // GET a single user by its _id and populated thought and friend data
 
-// POST a new user:
+// User.findOne()
+
+// POST a new user
+
+// User.create()
 
 // PUT to update a user by its _id
 
+// User.findOneAndUpdate()
+
 // DELETE to remove user by its _id
+
+// User.findOneAndDelete()
 
 // BONUS: Remove a user's associated thoughts when deleted.
 
 // -------------routes for api/users/:userId/friends/:friendId-------------
 
 // POST to add a new friend to a user's friend list
+
 
 // DELETE to remove a friend from a user's friend list
 
