64281c5383fa2b4d4ab2f5ddcd63d5ee752c3591
Rucatues <115330764+Rucatues@users.noreply.github.com>
2023-04-19 10:12:52 -0500

    virtuals true added to User model

--- models/User.js
+++ models/User.js
@@ -13,15 +13,22 @@ const userSchema = new mongoose.Schema({
         unique: true,
         match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
     },
-    thoughts: [],
     // Array of _id values referencing the Thought model
+    thoughts: [{ type: Schema.Types.ObjectId, ref: "thought" }],
 
-    friends: []
     // Array of _id values referencing the User model (self-reference)
+    friends: [{ type: Schema.Types.ObjectId, ref: "user" }]
 
-});
+},
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    });
 
 // Schema Settings
+
 // Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
 
 userSchema.virtual('friendCount').get(function () {
